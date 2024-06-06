78362a2c73df449ac9d2535fa604d0215c6417eb
xdedeone <xdedede1@gmail.com>
2024-06-05 19:09:58 -0500

    Added progress task indicator

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ function addTask() {
     if(inputBox.value === "")
         alert("You need to add some task!")
     else {
+        let inProgressTasks = listContainer.querySelectorAll(".in-progress");
+        let lastChild = inProgressTasks[inProgressTasks.length -1];
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        lastChild ? lastChild.after(li) : listContainer.prepend(li);
         let span = document.createElement("span");
         li.appendChild(span);
         let img = document.createElement("img");
@@ -21,11 +23,27 @@ function addTask() {
 }
 
 listContainer.addEventListener("click", function(e) {
+    let inProgressTasks = listContainer.querySelectorAll(".in-progress");
+    let lastChild = inProgressTasks[inProgressTasks.length -1];
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }
-    else if (e.target.parentElement.tagName === "SPAN")
-        e.target.parentElement.parentElement.remove();
+        if (e.target.classList.contains("checked")){
+            e.target.classList.remove("checked")
+            lastChild ? lastChild.after(e.target) : listContainer.prepend(e.target);
+        }
+        else if(e.target.classList.contains("in-progress")){
+            e.target.classList.replace("in-progress", "checked");
+            listContainer.append(e.target);
+        }
+        else{
+            e.target.classList.add("in-progress");
+            listContainer.prepend(e.target);
+        }
+        
+    } else if (e.target.parentElement.tagName === "SPAN")
+            e.target.parentElement.parentElement.remove();
+
+    Array.from(listContainer).sort();
+
     saveData();
 }, false);
 
