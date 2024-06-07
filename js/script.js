641312a07b685b6697d3735bf4a3ef390c282e77
xdedeone <xdedede1@gmail.com>
2024-06-06 21:39:21 -0500

    Added tags creation and filter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
 //
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const categoryTags = document.getElementById("category-tags");
+
+function newCategory() {
+    let button = document.createElement("button");
+    button.setAttribute("class", "tablink")
+    let categoryName = prompt("Tag Name")
+    if (categoryName == "" || !categoryName)
+        return
+    else {
+        button.innerHTML = categoryName.toUpperCase();
+        categoryTags.append(button);
+    }
+    setEvents();
+    saveData();
+}
+
+function clearTags() {
+    let allTag = document.getElementById("all");
+    let selectedTags = document.querySelectorAll(".selected").forEach(t => t.classList.remove("selected"));
+    allTag.classList.add("selected")
+}
+
+function selectTab(e) {
+    let allTag = document.getElementById("all");
+    if(allTag.classList.contains("selected"))
+        allTag.classList.remove("selected");
+    e.target.classList.toggle("selected");
+}
+
+function longPressed(e) {
+    window.addEventListener('click', captureClick, true);
+    confirm("You want to delete this TAG?") ? e.target.remove() : 0;
+    saveData();
+}
+
+function captureClick(e) {
+    e.stopPropagation();
+    window.removeEventListener('click', captureClick, true);
+}
 
 function addTask() {
     if(inputBox.value === "")
@@ -40,7 +79,8 @@ listContainer.addEventListener("click", function(e) {
         }
         
     } else if (e.target.parentElement.tagName === "SPAN")
-            e.target.parentElement.parentElement.remove();
+            confirm("You want to delete this task?") ? e.target.parentElement.parentElement.remove() : 0;
+        
 
     Array.from(listContainer).sort();
 
@@ -48,11 +88,26 @@ listContainer.addEventListener("click", function(e) {
 }, false);
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem("taskData", listContainer.innerHTML);
+    localStorage.setItem("tagData", categoryTags.innerHTML);
 }
 
 function loadData() {
-    listContainer.innerHTML = localStorage.getItem("data");
+    listContainer.innerHTML = localStorage.getItem("taskData");
+    categoryTags.innerHTML = localStorage.getItem("tagData");
+    setEvents();
+}
+
+function setEvents() {
+    tagButtons = document.querySelectorAll(".tablink");
+    for (let i = 1; i < tagButtons.length; i++) {
+        tagButtons[i].addEventListener("mousedown", function(e) {
+            pressTimer = window.setTimeout(() => longPressed(e), 1000) });
+        tagButtons[i].addEventListener("mouseup", function() {
+            clearTimeout(pressTimer) });
+        tagButtons[i].addEventListener("click", function(e) {
+            selectTab(e) });
+    };
 }
 
 loadData();
